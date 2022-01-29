84e1a8771db90063e7db80f7576b538cb8f1234a
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-01-29 15:56:09 +0100

    Feat: Add user registration

--- users/views.py
+++ users/views.py
@@ -1,22 +1,23 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             #message.debug
             #message.info
             #message.success
             #message.warning
             #message.error
+            form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Acount created for {username}!')
+            messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
