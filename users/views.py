25fc4bfd979f24df898f867a7f19857ec36f4d05
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-02-04 00:14:21 +0100

    Feat: Add user profile

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm
 
@@ -9,15 +10,20 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            #message.debug
-            #message.info
-            #message.success
-            #message.warning
-            #message.error
+            # message.debug
+            # message.info
+            # message.success
+            # message.warning
+            # message.error
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account as been created! You can now login')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
