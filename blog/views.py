c9e593103f09303b958af339bfed8fe998b196d8
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-03-04 16:22:43 +0100

    Feat: add new features

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,37 @@
 from django.shortcuts import render
 from .models import Post
-
-posts = [
-    {
-        'author': 'Sten',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'December 12, 2056',
-    },
-    {
-        'author': 'Chizoba',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'February 12, 2057',
-    }
-]
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+)
 
 
-# Create your views here.
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
