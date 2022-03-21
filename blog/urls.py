94c6a216e6d78813420b1624a24636b0d0f47cb7
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-03-21 10:41:28 +0100

    Feat: Add pagination

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
     PostListView,
+    UserPostListView,
     PostDetailView,
     PostCreateView,
     PostUpdateView,
@@ -10,6 +11,7 @@ from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
